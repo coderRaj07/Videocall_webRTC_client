647fe7c484117f5d40bb8a400fd9c0be7b5d90b9
Rajendra Bisoi <90323977+coderRaj07@users.noreply.github.com>
2023-12-30 21:16:32 +0530

    SocketProvider.jsx

--- src/context/SocketProvider.jsx
+++ src/context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("localhost:8000"), []);
+  const socket = useMemo(() => io("localhost:8000" || "videocall_webrtc_server.railway.internal"), []);
 
   return (
     <SocketContext.Provider value={socket}>
